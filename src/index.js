8103875f60953d206986e67c2cd64204b93275e2
Sujal <sujal.kumar311@gmail.com>
2024-04-27 14:42:07 +0530

    Setup simple and basic middlewares

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const responseTime = require("response-time")
+
 const { PORT } = require("./config/server_config.js")
 
 const ApiRouter = require("./routes/api_router.js");
 
+
 const app = express();
+
+// app.use(responseTime(function f(req, res, time) {
+//     console.log("Time Elapsed : ", time);
+//     res.setHeader("X-Response-Time", time)
+// }))
+app.use(responseTime())
+
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/api", ApiRouter) // if any req. comes with url starting with /api
 
--- src/routes/v1/ping_router.js
+++ src/routes/v1/ping_router.js
@@ -3,6 +3,28 @@ const { pingController } = require("../../controllers/ping_controller")
 
 const router = express.Router();
 
-router.get("/", pingController); // mapping a route to a controller
+// inbuilt middle ware 
+function checkRequest(req, res, next) {
+    console.log("Checking the request")
+
+    next();
+    console.log("Endin vro1")
+}
+
+function logger(req, res, next) {
+    console.log("Logging request")
+    next();
+    console.log("Endin vro2")
+
+}
+
+function authcheck(req, res, next) {
+    console.log("Auth check")
+    next();
+    console.log("Endin vro3")
+
+}
+
+router.get("/", [checkRequest, logger, authcheck], pingController); // mapping a route to a controller
 
 module.exports = router;
