7aa717e99291db6a40bc35ae5e1eef12a6c895ef
Sujal <sujal.kumar311@gmail.com>
2024-04-27 16:43:29 +0530

    Added basic in memery db based service

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -1,8 +1,12 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes")
 
+const ProductService = require("../services/product_service")
+
 function createProduct(req, res) {
     try {
         // some db processing 
+
+        const response = ProductService.createProduct(req.body);
 
         return res
             .status(StatusCodes.CREATED)
@@ -10,21 +14,48 @@ function createProduct(req, res) {
                 success: true,
                 error: {},
                 message: ReasonPhrases.CREATED + " Product Successfully",
-                data: {
-                    id: Math.random() * (20),
-                    title: req.body.title,
-                    description: req.body.description,
-                    category: req.body.category,
-                    price: req.body.price,
-                    image: req.body.image
-                }
-
+                data: response
             })
     } catch (error) {
         console.log("Something went wrong", error)
     }
 }
 
+function getProducts(req, res) {
+    try {
+        const response = ProductService.getProducts();
+        return res
+            .status(StatusCodes.OK)
+            .json({
+                success: true,
+                error: {},
+                message: "Successfully fetched the products",
+                data: response
+            })
+    } catch (error) {
+
+    }
+}
+
+
+function getProduct(req, res) {
+    try {
+        const response = ProductService.getProduct(req.params.id);
+        return res
+            .status(StatusCodes.OK)
+            .json({
+                success: true,
+                error: {},
+                message: "Successfully fetched the Product",
+                data: response
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
-    createProduct
+    createProduct,
+    getProducts,
+    getProduct
 }
